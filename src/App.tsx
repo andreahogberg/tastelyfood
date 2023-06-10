7dfaeea57efa5218aacd4629a52cf17c1c88c8e1
Andrea Högberg <andreahogberg@live.se>
2023-06-10 15:52:57 +0200

    Fixed backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,59 @@
-import React, { useState } from "react";
-import axios from "axios";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import "./index.css";
-import HamburgerMenu from "./components/HamburgerMenu";
-import RecipeCard from "./components/RecipeCard";
-import MainApp from "./components/MainApp";
-import sven from "./assets/hidethepainharold.jpg";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Profile from "./components/Profile";
-import RecipeUpload from "./components/RecipeUpload";
-import Upload from "./components/Upload";
-import Discover from "./components/Discover";
-import SavedPageMobile from "./components/SavedPageMobile";
-import LandingPage from "./components/LandingPage";
-import LandingPageTablet from "./components/LandingPageTablet";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import MainPage from "./components/MainPage";
-import MainPageTablet from "./components/MainPageTablet";
-import RecipePage from "./components/RecipePage";
+import React, { useState } from "react"
+import axios from "axios"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+import "./index.css"
+import HamburgerMenu from "./components/HamburgerMenu"
+import RecipeCard from "./components/RecipeCard"
+import MainApp from "./components/MainApp"
+import sven from "./assets/hidethepainharold.jpg"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Profile from "./components/Profile"
+import RecipeUpload from "./components/RecipeUpload"
+import Upload from "./components/Upload"
+import Discover from "./components/Discover"
+import SavedPageMobile from "./components/SavedPageMobile"
+import LandingPage from "./components/LandingPage"
+import LandingPageTablet from "./components/LandingPageTablet"
+import Login from "./components/Login"
+import SignUp from "./components/SignUp"
+import MainPage from "./components/MainPage"
+import MainPageTablet from "./components/MainPageTablet"
+import RecipePage from "./components/RecipePage"
 
 function App() {
-  const [file, setFile] = useState(null);
-  const [uploadedFile, setUploadedFile] = useState(null);
+  const [file, setFile] = useState(null)
+  const [uploadedFile, setUploadedFile] = useState(null)
 
   const fileSelectedHandler = (event) => {
-    setFile(event.target.files[0]);
-  };
+    setFile(event.target.files[0])
+  }
 
   const fileUploadHandler = async () => {
-    const formData = new FormData();
-    formData.append("file", file);
+    const formData = new FormData()
+    formData.append("file", file)
 
     try {
       const res = await axios.post("http://localhost:8080/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
-      });
+      })
 
-      const { fileName, filePath } = res.data;
-      setUploadedFile({ fileName, filePath });
+      const { fileName, filePath } = res.data
+      setUploadedFile({ fileName, filePath })
     } catch (err) {
       if (err.response && err.response.status === 500) {
-        console.log("There was a problem with the server");
+        console.log("There was a problem with the server")
       } else if (err.response && err.response.data && err.response.data.msg) {
-        console.log(err.response.data.msg);
+        console.log(err.response.data.msg)
       } else {
-        console.log("An error occurred while uploading the file");
+        console.log("An error occurred while uploading the file")
       }
     }
-  };
+  }
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   const recipe = {
     id: "1",
@@ -67,8 +67,12 @@ function App() {
     servings: 4,
     cookTime: 30,
     ingredients: ["Ingredient 1", "Ingredient 2", "Ingredient 3"],
-    instructions: ["Step 1: Do this", "Step 2: Do that", "Step 3: Finally, do this"],
-  };
+    instructions: [
+      "Step 1: Do this",
+      "Step 2: Do that",
+      "Step 3: Finally, do this",
+    ],
+  }
 
   return (
     <Router>
@@ -81,7 +85,13 @@ function App() {
           <Route
             path="/profile"
             element={
-              <Profile user={{ name: recipe.creator.name, avatar: recipe.creator.avatar, favoriteRecipes: [] }} />
+              <Profile
+                user={{
+                  name: recipe.creator.name,
+                  avatar: recipe.creator.avatar,
+                  favoriteRecipes: [],
+                }}
+              />
             }
           />
           <Route path="/recipe/:id" element={<RecipePage recipe={recipe} />} />
@@ -96,7 +106,11 @@ function App() {
         </Routes>
         <div className="top-links">
           <Link to="/profile" className="small-avatar-link">
-            <img src={recipe.creator.avatar} className="small-avatar" alt={recipe.creator.name} />
+            <img
+              src={recipe.creator.avatar}
+              className="small-avatar"
+              alt={recipe.creator.name}
+            />
           </Link>
           {/* <Link to="/upload" className="upload-recipe"></Link> */}
           <Link to="/discover" className="discover-recipe"></Link>
@@ -106,7 +120,11 @@ function App() {
         <Link to="/login" className="login-link"></Link>
         <h1 className="App-header">File upload</h1>
         <section>
-          <input className="App-input" type="file" onChange={fileSelectedHandler} />
+          <input
+            className="App-input"
+            type="file"
+            onChange={fileSelectedHandler}
+          />
           <button className="App-button" onClick={fileUploadHandler}>
             Upload
           </button>
@@ -114,13 +132,19 @@ function App() {
         {uploadedFile && (
           <div className="upload">
             <h3 className="upload_header">{uploadedFile.fileName}</h3>
-            <img className="upload_image" src={uploadedFile.filePath} alt="logo" />
+            <img
+              className="upload_image"
+              src={uploadedFile.filePath}
+              alt="logo"
+            />
           </div>
         )}
-        <p className="read-the-docs">Click on the Vite and React logos to learn more.</p>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more.
+        </p>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,60 +1,71 @@
-import React, { useState } from 'react';
-import './RecipeCard.css';
-import { Link, useNavigate } from 'react-router-dom';
-import bookmarkIcon from '../assets/bookmark-icon.svg';
+import React, { useState } from "react"
+import "./RecipeCard.css"
+import { Link, useNavigate } from "react-router-dom"
+import bookmarkIcon from "../assets/bookmark-icon.svg"
 
 interface RecipeCardProps {
   recipe: {
-    id: string;
-    name: string;
-    description: string;
-    image: string;
+    id: string
+    name: string
+    description: string
+    image: string
     creator: {
-      name: string;
-      avatar: string;
-    };
-  };
+      name: string
+      avatar: string
+    }
+  }
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
-  const [isBookmarked, setIsBookmarked] = useState(false);
-  const navigate = useNavigate();
+  const [isBookmarked, setIsBookmarked] = useState(false)
+  const navigate = useNavigate()
 
   const handleBookmarkToggle = () => {
-    setIsBookmarked((prevIsBookmarked) => !prevIsBookmarked);
-  };
+    setIsBookmarked((prevIsBookmarked) => !prevIsBookmarked)
+  }
 
   const handleClick = () => {
-    navigate(`/recipes/${recipe.id}`);
-  };
+    navigate(`/recipes/${recipe.id}`)
+  }
 
   const handleUploadClick = () => {
-    navigate('/upload');
-  };
+    navigate("/upload")
+  }
 
   return (
     <div className="recipe-card" onClick={handleClick}>
       <div className="recipe-card-header">
         <Link to="/profile">
-          <img className="recipe-card-avatar" src={recipe.creator.avatar} alt={recipe.creator.name} />
+          <img
+            className="recipe-card-avatar"
+            src={recipe.creator.avatar}
+            alt={recipe.creator.name}
+          />
         </Link>
         <span className="recipe-card-creator">{recipe.creator.name}</span>
       </div>
       <div className="recipe-card-content">
-        <img className="recipe-card-image" src={recipe.image} alt={recipe.name} />
+        <img
+          className="recipe-card-image"
+          src={recipe.image}
+          alt={recipe.name}
+        />
         <h3 className="recipe-card-title">{recipe.name}</h3>
         <p className="recipe-card-description">{recipe.description}</p>
         <div className="recipe-card-footer"></div>
       </div>
-      <button className={`bookmark-button ${isBookmarked ? 'bookmarked' : ''}`} onClick={handleBookmarkToggle}>
+      <button
+        className={`bookmark-button ${isBookmarked ? "bookmarked" : ""}`}
+        onClick={handleBookmarkToggle}
+      >
         <img className="bookmark-icon" src={bookmarkIcon} alt="Bookmark" />
-        {isBookmarked ? 'Bookmarked' : 'Bookmark'}
+        {isBookmarked ? "Bookmarked" : "Bookmark"}
       </button>
       <button className="upload-button" onClick={handleUploadClick}>
         Upload Recipe
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default RecipeCard;
+export default RecipeCard
